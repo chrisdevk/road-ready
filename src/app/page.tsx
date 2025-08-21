2ba4c4c888fdcc043ce8aa7823fb7a76e333c5f1
shadow16305 <chris16305@gmail.com>
2025-08-21 16:12:29 +0200

    init dev branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Heading</h1>
+      <h1>Hello world</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
       </p>
