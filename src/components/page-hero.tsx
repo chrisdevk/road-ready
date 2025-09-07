8d3d1eb7ded61c7131c9ab093e4b6908e3d0de0d
shadow16305 <chris16305@gmail.com>
2025-09-07 15:53:59 +0200

    feat: add faq page

--- src/components/page-hero.tsx
+++ src/components/page-hero.tsx
@@ -23,7 +23,7 @@ export const PageHero = ({ heading, subheading, color }: PageHeroProps) => {
     <section className={cn("py-32 mt-20", getColor(color))}>
       <Container className="text-white text-center flex flex-col gap-y-10 items-center justify-center ">
         <h1>{heading}</h1>
-        <p>{subheading}</p>
+        <p dangerouslySetInnerHTML={{ __html: subheading }} />
       </Container>
     </section>
   );
