9b6af5309cfbdf113e127a090c442cdd7e27245d
shadow16305 <chris16305@gmail.com>
2025-10-18 16:58:23 +0200

    feat: add metadata & ISR to pages

--- src/app/packages/page.tsx
+++ src/app/packages/page.tsx
@@ -6,6 +6,39 @@ import { Location } from "@/components/location";
 import { PageHero } from "@/components/page-hero";
 import { Container } from "@/components/ui/container";
 import packages from "@/utils/data/static/packages.json";
+import { Metadata } from "next";
+
+export const revalidate = 3600; // 1 hour
+
+export function generateMetadata(): Metadata {
+  return {
+    title: "Driving Lesson Packages - Road Ready Driving School",
+    description:
+      "Choose the perfect driving lesson package for your needs. Learn at your own pace with DMV-licensed instructors, and build the skills and confidence to drive safely and independently",
+    keywords:
+      "Driving lesson packages, DMV-licensed instructors, driving lessons, driving test, driving license, driver's education, driver's training, driver's ed, driver's education program, driver's training program, driver's ed program, driver's education course, driver's training course, driver's ed course, driver's education program, driver's training program, driver's ed program, driver's education course, driver's training course, driver's ed course",
+    authors: [{ name: "Road Ready Driving School" }],
+    robots: {
+      index: true,
+      follow: true,
+    },
+    openGraph: {
+      title: "Driving Lesson Packages - Road Ready Driving School",
+      description:
+        "Choose the perfect driving lesson package for your needs. Learn at your own pace with DMV-licensed instructors, and build the skills and confidence to drive safely and independently",
+      url: "https://roadready.com",
+      siteName: "Road Ready Driving School",
+      images: [
+        {
+          url: "/images/hero-img.jpg",
+          width: 1200,
+          height: 630,
+          alt: "Road Ready Driving School",
+        },
+      ],
+    },
+  };
+}
 
 export default async function PackagesPage() {
   const data = packages;
