47d484a058f57506480b448461d06f52a792ad56
shadow16305 <chris16305@gmail.com>
2025-11-04 13:38:29 +0100

    update: add metadata and force-static to policies page

--- src/app/our-policies/page.tsx
+++ src/app/our-policies/page.tsx
@@ -2,6 +2,16 @@ import { PageHero } from "@/components/page-hero";
 import { Container } from "@/components/ui/container";
 import { policies as policiesData } from "@/utils/data/static/policies.json";
 
+export const dynamic = "force-static";
+
+export const generateMetadata = () => {
+  return {
+    title: "RoadReady Driving School Policies",
+    description:
+      "Please read our policies, which apply to the driver training services offered by RoadReady Driving  School. By using our services, you agree to these terms and conditions.",
+  };
+};
+
 export default function OurPoliciesPage() {
   const data = policiesData;
   return (
